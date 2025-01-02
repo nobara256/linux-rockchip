f3b161a46153db6ba6da1e898f27ccfb05d0d208
Arunava Das <arunavadas511000@gmail.com>
2025-01-02 12:53:10 +0530

    10

--- package-array.py
+++ package-array.py
@@ -6,16 +6,21 @@ from datetime import datetime
 payload = loads(argv[1])
 
 
+package_done_already = {}
+
+
 def kernel_ver_selector(package):
     return (
-        package["moniker"] == "mainline" or
-        package["version"] == payload["latest_stable"]["version"]
+        (package["moniker"] == "mainline" or package["moniker"] == "stable") and
+        (package["version"] not in package_done_already)
     )
 
+
+payload_array = list(filter(kernel_ver_selector, payload["releases"]))
+
+
 print(
-    "array="+dumps({
-            "include": list(filter(kernel_ver_selector, payload["releases"]))
-        },
+    "array="+dumps({"include": payload_array},
     indent=None
     ),
     end=""

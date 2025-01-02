443e7d6637e7da66fdde65b946ec2ddee53f1c51
Arunava Das <arunavadas511000@gmail.com>
2025-01-02 13:33:27 +0530

    15

--- package-array.py
+++ package-array.py
@@ -6,19 +6,24 @@ from datetime import datetime
 payload = loads(argv[1])
 
 
-package_done_already = {}
+# Append package versions to be skipped.
+ignore_kernel_package = {
+    "6.12.7",
+    "6.11.11"
+}
 
 
-def kernel_ver_selector(package):
+
+def kernel_package_selector(package):
     return (
         (package["moniker"] == "mainline" or package["moniker"] == "stable") and
-        (package["version"] not in package_done_already)
+        (package["version"] not in ignore_kernel_package)
     )
 
+# Packages into a list.
+payload_array = list(filter(kernel_package_selector, payload["releases"]))
 
-payload_array = list(filter(kernel_ver_selector, payload["releases"]))
-
-
+# JSON would be used to create the matrix.
 print(
     "array="+dumps({"include": payload_array},
     indent=None
